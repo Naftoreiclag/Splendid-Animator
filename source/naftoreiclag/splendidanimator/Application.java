0a5ce222f342d1a576ca23c4e962bcb8c9b16304
Naftoreiclag <naftoreiclag@gmail.com>
2014-01-25 21:53:27 -0800

    Preparing for adding other libraries
    Licenses give me a headache.

--- source/naftoreiclag/splendidanimator/Application.java
+++ source/naftoreiclag/splendidanimator/Application.java
@@ -1,12 +1,18 @@
+/* Copyright (c) 2014 "Naftoreiclag" https://github.com/Naftoreiclag
+ *
+ * Distributed under the MIT License (http://opensource.org/licenses/mit-license.html)
+ * See accompanying file LICENSE
+ */
+
 package naftoreiclag.splendidanimator;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 public class Application extends JFrame
 {
@@ -15,10 +21,9 @@ public class Application extends JFrame
 	public Application()
 	{
 		JPanel panel = new JPanel();
-		
 
 		JButton button = new JButton("Swag");
-		
+
 		button.addActionListener(new ActionListener()
 		{
 			@Override
@@ -29,7 +34,7 @@ public class Application extends JFrame
 		});
 
 		panel.add(button);
-		
+
 		this.getContentPane().add(panel);
 		this.setTitle("Splendid Animator");
 		this.setSize(800, 600);
@@ -39,14 +44,56 @@ public class Application extends JFrame
 
 	public static void main(String[] args)
 	{
+		// Set up look and feel
+		setLookAndFeel();
+		
+		// Make our window
 		SwingUtilities.invokeLater(new Runnable()
 		{
 			@Override
 			public void run()
 			{
+				// Use standard window decoration
+				JFrame.setDefaultLookAndFeelDecorated(false);
+				
+				// Start application
 				Application application = new Application();
 				application.setVisible(true);
 			}
 		});
+	}
+
+	// Try set it to be Seaglass. If that fails make it Nimbus. If that fails make it Metal.
+	public static void setLookAndFeel()
+	{
+		try
+		{
+			UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
+		}
+		catch (Exception e1)
+		{
+			try
+			{
+				for(UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels())
+				{
+					if(info.getName().equals("Nimbus"))
+					{
+						UIManager.setLookAndFeel(info.getClassName());
+						break;
+					}
+				}
+			}
+			catch (Exception e2)
+			{
+				try
+				{
+					UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+				}
+				catch (Exception e3)
+				{
+					e3.printStackTrace();
+				}
+			}
+		}
 	}
 }
