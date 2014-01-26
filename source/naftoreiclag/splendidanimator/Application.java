5b88a151cf892ec715dcc7d5e7283ef636337eb3
Naftoreiclag <naftoreiclag@gmail.com>
2014-01-25 22:51:42 -0800

    Changed to lwjgl app

--- source/naftoreiclag/splendidanimator/Application.java
+++ source/naftoreiclag/splendidanimator/Application.java
@@ -6,94 +6,35 @@
 
 package naftoreiclag.splendidanimator;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 
-public class Application extends JFrame
+public class Application
 {
-	private static final long serialVersionUID = 6474492987861402629L;
-
-	public Application()
+	public void start()
 	{
-		JPanel panel = new JPanel();
-
-		JButton button = new JButton("Swag");
-
-		button.addActionListener(new ActionListener()
+		try
 		{
-			@Override
-			public void actionPerformed(ActionEvent event)
-			{
-				System.exit(0);
-			}
-		});
+			Display.setDisplayMode(new DisplayMode(800, 600));
+			Display.create();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			System.exit(0);
+		}
 
-		panel.add(button);
+		while (!Display.isCloseRequested())
+		{
+			Display.update();
+		}
 
-		this.getContentPane().add(panel);
-		this.setTitle("Splendid Animator");
-		this.setSize(800, 600);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		Display.destroy();
 	}
 
 	public static void main(String[] args)
 	{
-		// Set up look and feel
-		setLookAndFeel();
-		
-		// Make our window
-		SwingUtilities.invokeLater(new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				// Use standard window decoration
-				JFrame.setDefaultLookAndFeelDecorated(false);
-				
-				// Start application
-				Application application = new Application();
-				application.setVisible(true);
-			}
-		});
-	}
-
-	// Try set it to be Seaglass. If that fails make it Nimbus. If that fails make it Metal.
-	public static void setLookAndFeel()
-	{
-		try
-		{
-			UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
-		}
-		catch (Exception e1)
-		{
-			try
-			{
-				for(UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels())
-				{
-					if(info.getName().equals("Nimbus"))
-					{
-						UIManager.setLookAndFeel(info.getClassName());
-						break;
-					}
-				}
-			}
-			catch (Exception e2)
-			{
-				try
-				{
-					UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-				}
-				catch (Exception e3)
-				{
-					e3.printStackTrace();
-				}
-			}
-		}
+		Application application = new Application();
+		application.start();
 	}
 }
